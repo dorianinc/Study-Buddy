fd2c92801875bf2732cf16dc0475ada8f35cb914
Josh van Eyken <josh.vaneyken@gmail.com>
2024-07-22 12:43:46 -0700

    replace vanilla redux with redux toolkit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import React from "react";
 import { Switch, Route } from "react-router-dom";
-import * as sessionActions from "./store/sessionReducer";
-import Navigation from "./components/Navigation";
 import HomePage from "./components/HomePage/HomePage";
 
 function App() {
-  const dispatch = useDispatch();
-  const [isLoaded, setIsLoaded] = useState(false);
-  useEffect(() => {
-    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
-  }, [dispatch]);
 
   return (
     <div className="app-container">
-      <Navigation isLoaded={isLoaded} />
-      {isLoaded && (
         <div className="content-container">
           <Switch>
             <Route path="/">
@@ -23,7 +13,6 @@ function App() {
             </Route>
           </Switch>
         </div>
-      )}
     </div>
   );
 }

9c16a1451d75d9284d9cb8c56b3183490c42bb8a
Dorian Macias <dorianrdba@gmail.com>
2024-07-22 21:06:00 -0700

    declared authorId as the foreign key in document table

--- backend/routes/api/notes.js
+++ backend/routes/api/notes.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const { restoreUser, requireAuth, isAuthorized } = require("../../utils/auth");
-const {  doesNotExist } = require("../../utils/helpers.js");
+const { doesNotExist } = require("../../utils/helpers.js");
 const { Document } = require("../../db/models");
 
 const router = express.Router();
 
 // Create a note
 router.post("/", [restoreUser, requireAuth], async (req, res) => {
-  const docId = await Document.findByPk(req.query.docId);
-
+  console.log("creating notes")
+  const doc = await Document.findByPk(req.query.docId);
+  const docId = doc.id;
+  console.log("🖥️   docId: ",  docId)
 });
 module.exports = router;

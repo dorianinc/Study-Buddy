f75defcdb8812a6899816d8b01aa58f7b988e01b
Dorian Macias <dorianrdba@gmail.com>
2024-07-22 18:38:41 -0700

    made slight adjustment in notes.js for routes

--- backend/routes/api/notes.js
+++ backend/routes/api/notes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const { restoreUser, requireAuth, isAuthorized } = require("../../utils/auth");
 const {  doesNotExist } = require("../../utils/helpers.js");
+const { Document } = require("../../db/models");
 
 const router = express.Router();
 
 // Create a note
 router.post("/", [restoreUser, requireAuth], async (req, res) => {
-  const docId = await ReviewImage.findByPk(req.query.docId);
+  const docId = await Document.findByPk(req.query.docId);
 
 });
 module.exports = router;

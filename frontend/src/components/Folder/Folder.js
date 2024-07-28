5ee6206ffa641405cfa4a7bcff29f3cbdbf8815b
Josh van Eyken <josh.vaneyken@gmail.com>
2024-07-27 17:46:35 -0700

    Add styling and text to folder component

--- frontend/src/components/Folder/Folder.js
+++ frontend/src/components/Folder/Folder.js
@@ -1,7 +1,30 @@
+import { 
+    Box,
+    Text
+} from "@chakra-ui/react";
+
 function Folder() {
-    return <>
-    <div>I am the Folder component;</div>
-    </>
+    const folder = {
+        name: "Folder Name Folder Name",
+    }
+    return (
+        <>
+            <Box as="button" 
+                boxShadow="lg" 
+                rounded='xl' 
+                bg='white' 
+                color='black'
+                px={2}
+                w={200} 
+                h={200} 
+                _hover={{
+                    bg: "lightgrey"
+                }}
+            >
+                <Text noOfLines={1}>{folder.name}</Text>
+            </Box>
+        </>
+    )
 }
 
 export default Folder;

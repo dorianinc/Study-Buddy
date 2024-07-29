b2ebd0f1910ab9fbd395c776b0a4e81073e11a81
Josh van Eyken <josh.vaneyken@gmail.com>
2024-07-29 10:34:12 -0700

    Add basic structure for NewFolderModal

--- frontend/src/components/Modals/NewFolderModal.js
+++ frontend/src/components/Modals/NewFolderModal.js
@@ -1,9 +1,46 @@
+  import {
+    FormErrorMessage,
+    FormLabel,
+    FormControl,
+    Input,
+    Button,
+    FormHelperText,
+  } from '@chakra-ui/react'
+
   function NewFolderModal() {
     return(
         <>
-        <div>I am the New Folder Modal</div>
+        <form>
+      <FormControl >
+        <FormLabel htmlFor='name'>Folder name</FormLabel>
+        <Input
+          id='name'
+          placeholder='name'
+          size='lg'
+        />
+        <FormHelperText>Enter the name you would like to give the folder</FormHelperText>
+        <FormErrorMessage>
+          {/* {errors.name && errors.name.message} */}
+        </FormErrorMessage>
+        <FormLabel htmlFor='name'>Category</FormLabel>
+        <Input
+          id='category'
+          placeholder='category'
+        />
+        <FormErrorMessage>
+          {/* {errors.name && errors.name.message} */}
+        </FormErrorMessage>
+      </FormControl>
+      <Button 
+      mt={4} 
+      colorScheme='teal' 
+    //   isLoading={isSubmitting} 
+      type='submit'>
+        Submit
+      </Button>
+    </form>
         </>
     )
   }
 
-  export default NewFolderModal
+  export default NewFolderModal;

6bc95198a032a75c9e40291370a34da7ba14c8fb
Josh van Eyken <josh.vaneyken@gmail.com>
2024-07-29 15:28:38 -0700

    Pass user info to createFolder function

--- frontend/src/components/Folder/NewFolderButton.js
+++ frontend/src/components/Folder/NewFolderButton.js
@@ -1,14 +1,7 @@
 import { Box } from "@chakra-ui/react";
 import { AddIcon } from "@chakra-ui/icons";
-// import { useCreateFolderMutation } from "../../store/features/api";
 
 function NewFolderButton() {
-  // const [createFolder] = useCreateFolderMutation();
-
-  // function createNewFolder() {
-  //   createFolder({ name: "History", category: "Social" })
-  // }
-
   return (
     <>
       <Box
--- frontend/src/components/Modals/NewFolderModal.js
+++ frontend/src/components/Modals/NewFolderModal.js
@@ -1,46 +1,51 @@
-  import {
-    FormErrorMessage,
-    FormLabel,
-    FormControl,
-    Input,
-    Button,
-    FormHelperText,
-  } from '@chakra-ui/react'
+import { useState } from "react";
+import {
+  FormErrorMessage,
+  FormLabel,
+  FormControl,
+  Input,
+  Button,
+  FormHelperText,
+} from "@chakra-ui/react";
+import { useCreateFolderMutation } from "../../store/features/api";
 
-  function NewFolderModal() {
-    return(
-        <>
-        <form>
-      <FormControl >
-        <FormLabel htmlFor='name'>Folder name</FormLabel>
-        <Input
-          id='name'
-          placeholder='name'
-          size='lg'
-        />
-        <FormHelperText>Enter the name you would like to give the folder</FormHelperText>
-        <FormErrorMessage>
-          {/* {errors.name && errors.name.message} */}
-        </FormErrorMessage>
-        <FormLabel htmlFor='name'>Category</FormLabel>
-        <Input
-          id='category'
-          placeholder='category'
-        />
-        <FormErrorMessage>
-          {/* {errors.name && errors.name.message} */}
-        </FormErrorMessage>
-      </FormControl>
-      <Button 
-      mt={4} 
-      colorScheme='teal' 
-    //   isLoading={isSubmitting} 
-      type='submit'>
-        Submit
-      </Button>
-    </form>
-        </>
-    )
+function NewFolderModal() {
+  const [folderName, setFolderName] = useState('');
+  const [category, setCategory] = useState('');
+  const [createFolder, { isLoading }] = useCreateFolderMutation();
+
+  function createNewFolder() {
+      createFolder({ name: folderName, category: category });
   }
+  return (
+    <>
+      <form onSubmit={createNewFolder}>
+        <FormControl>
+          <FormLabel htmlFor="name">Folder name</FormLabel>
+          <Input id="name" placeholder="name" size="lg" value={folderName} onChange={(e) => setFolderName(e.target.value)}/>
+          <FormHelperText>
+            Enter the name you would like to give the folder
+          </FormHelperText>
+          <FormErrorMessage>
+            {/* {errors.name && errors.name.message} */}
+          </FormErrorMessage>
+          <FormLabel htmlFor="name">Category</FormLabel>
+          <Input id="category" placeholder="category" value={category} onChange={(e) => setCategory(e.target.value)}/>
+          <FormErrorMessage>
+            {/* {errors.name && errors.name.message} */}
+          </FormErrorMessage>
+        </FormControl>
+        <Button
+          mt={4}
+          colorScheme="teal"
+            isLoading={isLoading}
+          type="submit"
+        >
+          Submit
+        </Button>
+      </form>
+    </>
+  );
+}
 
-  export default NewFolderModal;
+export default NewFolderModal;

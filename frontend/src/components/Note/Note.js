044be97ca305ed313fab82a3dcdd4ff9b841438c
Josh van Eyken <josh.vaneyken@gmail.com>
2024-07-30 16:40:37 -0700

    Add basic structure and styling for Note component

--- frontend/src/components/Note/Note.js
+++ frontend/src/components/Note/Note.js
@@ -1,7 +1,79 @@
+import { useEffect, useRef } from "react";
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Button,
+  useDisclosure,
+  Input,
+} from "@chakra-ui/react";
+import { ArrowLeftIcon } from "@chakra-ui/icons";
+
 function Note() {
+  const document = {
+    name: "Math homework",
+  };
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = useRef();
+
+  useEffect(() => {
+    const handleResize = () => {
+      // height of Navigation component
+      const navigationHeight = 50;
+      const middleHeight = window.innerHeight / 2 - navigationHeight;
+      btnRef.current.style.bottom = `${middleHeight}px`;
+    };
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <>
-      <div>I am the Note component</div>
+      <Button
+        ref={btnRef}
+        leftIcon={<ArrowLeftIcon />}
+        colorScheme="teal"
+        iconSpacing={1}
+        variant="outline"
+        borderRadius={50}
+        aria-label="Open Notes"
+        fontSize="20px"
+        position="fixed"
+        right="50px"
+        onClick={onOpen}
+      >
+        Notes
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        placement="right"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+        size="md"
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>{document.name} Notes</DrawerHeader>
+
+          <DrawerBody>
+            <Input placeholder="Type here..." />
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant="outline" mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme="blue">Save</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
     </>
   );
 }

f990cc895171c7277b83d00aadfd70a94ce2d032
Wilmer <wilmer.sampedro@gmail.com>
2024-07-30 13:15:44 -0700

    in progress

--- frontend/src/components/Documents/Documents.js
+++ frontend/src/components/Documents/Documents.js
@@ -2,22 +2,25 @@ import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Container, Grid } from "@chakra-ui/react";
 import { useGetFoldersMutation, useGetOneFolderMutation } from "../../store/features/api";
+import { NavLink } from "react-router-dom";
 
 function Documents() {
   const [ getOneFolder ] = useGetOneFolderMutation();
   const folderIdTest = 1
   const folder = useSelector(state => state.folder[folderIdTest])
   console.log("🚀 ~ Documents ~ folder:", folder)
-  console.log("folder docs: ", folder.documents)
+  const documents = folder?.documents
+  console.log("folder docs: ", folder?.documents)
   // const documents = folder.documents;
   // console.log("🚀 ~ Documents ~ documents:", documents)
   // const documents = folder.documents;
   // console.log("🚀 ~ Documents ~ documents:", documents)
   useEffect(() => {
     getOneFolder(folderIdTest);
-  }, [getOneFolder, folder])
+  }, [getOneFolder])
 
-  // if(!folder || !documents) return
+
+  if(!folder || !documents) return
   // console.log("🚀 ~ Documents ~ folder:", folder)
 
   // useEffect(() => {
@@ -28,14 +31,17 @@ function Documents() {
 
   // console.log(documents)
   return (
-    <Grid>
-      {/* {documents && (
-        documents.map(doc =>
-          <Container>
-            {document.id}
+    <Grid templateColumns='repeat(5, 1fr)' gap={6}>
+      {documents && (
+        documents?.map(doc =>
+          <NavLink to={`documents/${doc.id}`} key={doc.id}>
+
+          <Container >
+            {doc?.name}
           </Container>
+          </NavLink>
         )
-      )} */}
+      )}
     </Grid>
   )
 }

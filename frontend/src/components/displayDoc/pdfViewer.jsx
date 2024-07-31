4c9e5df8179b0c0c5aac11d45eac7788853e315b
An Ngo <anng0149@gmail.com>
2024-07-31 14:51:14 -0700

    pdf viewer

--- frontend/src/components/displayDoc/pdfViewer.jsx
+++ frontend/src/components/displayDoc/pdfViewer.jsx
@@ -6,9 +6,9 @@
 
 import { Outline, pdfjs } from 'react-pdf';
 import { useRef, useEffect, useState, useCallback } from 'react';
-import { Document, Page } from 'react-pdf';
+import { Document, Page} from 'react-pdf';
 // import { useResizeObserver } from 'wojtekmaj/react-hooks';
-// import pdfUrl from './self-service-course-4-project-kiva-robot-remote-control.pdf'
+import pdfUrl from './self-service-course-4-project-kiva-robot-remote-control.pdf'
 
 
 
@@ -22,38 +22,75 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 const options = {
     standardFontDataUrl: `https://unpkg.com/pdfjs-dist@${pdfjs.version}/standard_fonts`,
 };
-function PdfViewer({pdfUrl}) {
+function PdfViewer() {
     const [numPages, setNumPages] = useState();
     const [pageNumber, setPageNumber] = useState(1);
     const [doneLoading, setDoneLoading] = useState(false)
     // const [textContent,setTextcontent] = useState()
     const [selectedText, setSelectedText] = useState('')
-
-    const cnvRef = useRef()
-
-    console.log(cnvRef)
+    const divRef = useRef()
     let startX = useRef()
     let startY = useRef()
-
+    const canvasRef = useRef()
     // useEffect(()=>{
     //     if(!doneLoading) setDoneLoading(true)
 
     // },[doneLoading,selectedText])
+    divRef.current?.addEventListener('mousedown',(e)=>{
+        console.log(e)
+        startX.current = e.pageX
+        startY.current = e.pageY
+    })
 
+    divRef.current?.addEventListener('mouseup',(e)=>{
+        const text = window.getSelection().toString()
+        setSelectedText(text)
+        const endX = e.pageX
+        const endY = e.pageY
+        highlighting(startX.current,startY.current,endX,endY)
+    })
     function onDocumentLoadSuccess(pdf) {
         setNumPages(pdf._pdfInfo.numPages);
         setDoneLoading(true);
     }
-    function downClick(e) {
-        startX.current = e.offsetX
-        startY.current = e.offsetY
+
+    // const pdftester = document.querySelector('.react-pdf__Document')
+    console.log(pdfUrl)
+
+    async function highlighting(startX,startY,endX,endY){
+        console.log(`startX:${startX}, startY:${startY}, endX:${endX}, endY:${endY}`)
+        console.log(canvasRef.current)
+        const context = canvasRef.current.getContext('2d')
+        const width = endX - startX
+        const height = endY - startY
+        context.fillRect(startX,startY,width,height)
+        // const pdfPage = await document.querySelector('.textLayer')
+        // const content = await pdfPage?.children
+        // const selectWidth = endX - startX
+        // const selectHeight = endY - startY
+        // if(content){
+        //     Array.from(content).forEach(async child=>{
+        //         // let relativePosLeft = child.style.left.slice(child.style.left.length-8,child.style.left.length-3)
+        //         const topPx = await child.style.top
+        //         let relativePosTop = topPx.slice(topPx.length-8,topPx.length-3) - 70.90
+        //         // console.log(relativePosTop)
+        //         if(selectHeight > 0 && relativePosTop > startY && relativePosTop< startY + selectHeight){
+        //             console.log(child)
+        //             child.style.background = 'red'
+        //         }
+
+        //         Array.from(child.children).forEach(async textChild=>{
+        //             const topPx = await textChild.style.top
+        //             let relativePosTop = topPx.slice(topPx.length-8,topPx.length-3) - 70.90
+        //             if(selectHeight > 0 && relativePosTop> startY && relativePosTop < startY + selectHeight){
+        //                 console.log(textChild.innerHTML,relativePosTop)
+        //                 textChild.style.background = 'red'
+        //             }
+        //         })
+        //     })
+        // }
     }
 
-    async function mouseUp() {
-        const text = await window.getSelection().toString()
-        setSelectedText(text)
-        console.log(text)
-    }
     function prev() {
         if (pageNumber > 1) setPageNumber(prev => prev - 1)
     }
@@ -64,42 +101,12 @@ function PdfViewer({pdfUrl}) {
     function onItemClick({ pageNumber: itemPageNumber }) {
         setPageNumber(itemPageNumber);
     }
-    // const highlightPattern = (text, pattern) => {
-    //     console.log(text,',',pattern,text===pattern)
-    //     console.log(text.match(pattern))
-    //     return text.replace(pattern, (value) => `<div class='testing'>${value}</div>`);
-
-    // }
-    // const highlightPattern = (text, pattern) => {
-    //     const splitText = text.split(pattern)
-
-    //     if(splitText.length <= 1){
-    //         return text
-    //     }
-
-    //     const matches = text.match(pattern)
-
-    //     const result = splitText.reduce((arr,element,index)=>(matches[index] ? [
-    //         ...arr,
-    //         element,
-    //         `<mark>
-    //             ${matches[index]}
-    //         </mark>`
-    //     ]:[]))
-    //     console.log(result)
-    //     return result
-
-    // }
-
-    // const makeTextRenderer = useCallback(
-    //     (highlightText) => textItem => highlightPattern(textItem.str, highlightText),
-    //     [selectedText]
-    // )
-
 
     return (
         <div>{pdfUrl &&
-            <Document file={pdfUrl} onLoadSuccess={onDocumentLoadSuccess} options={options}>
+            <Document file={pdfUrl} onLoadSuccess={onDocumentLoadSuccess} options={options}
+                    inputRef={divRef}
+            >
                 <div className='content-table'>
                     <h2>Table of Content</h2>
                     <button id='prev' onClick={prev}>
@@ -110,13 +117,12 @@ function PdfViewer({pdfUrl}) {
                     </button>
                     <Outline
                         onItemClick={onItemClick}
-                    />
+                        />
 
                 </div>
                 <Page
                     pageNumber={pageNumber}
-                    canvasRef={cnvRef}
-                    onMouseUp={mouseUp}
+                    canvasRef={canvasRef}
                 >
 
                 </Page>

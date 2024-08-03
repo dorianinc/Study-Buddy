1cb5a170ccb868f8f6ab64a12529583c65d5d99d
Dorian Macias <dorianrdba@gmail.com>
2024-08-02 23:40:18 -0700

    merged notes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,9 @@ import { useRestoreUserQuery } from "./store/features/api";
 import MyFolders from "./components/Folder/MyFolders";
 import MyDocuments from "./components/Document/MyDocuments";
 import DocumentPageTest from "./components/DocumentPageTest/DocumentPageTest";
+import { useSelector } from "react-redux";
+import DisplayDoc from "./components/displayDoc/DisplayDoc";
+
 function App() {
   const user = useSelector((state) => state.session.user);
   const { isLoading } = useRestoreUserQuery();
@@ -12,6 +15,7 @@ function App() {
   return (
     <div className="app-container">
       <Navigation />
+      <DisplayDoc/>
       {user && (
         <div className="content-container">
           <Switch>

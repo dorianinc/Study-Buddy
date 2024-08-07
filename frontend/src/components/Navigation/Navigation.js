b0a8ddb7bcf3a0a4dc1fdcf9dad8a14043d59ad2
An Ngo <anng0149@gmail.com>
2024-08-07 08:50:03 -0700

    changed file nme in profile button

---
+++ frontend/src/components/Navigation/Navigation.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ProfileButton from "./ProfileButton/ProfileButton";
+import LogoButton from "./LogoButton/LogoButton";
+import { Flex, Text } from "@chakra-ui/react";
+
+function Navigation() {
+  const sessionUser = useSelector((state) => state.session.user);
+
+  return (
+    <Flex
+      justify="space-between"
+      borderBottom={"1px solid lightgrey"}
+      boxShadow={"sm"}
+      p={5}
+      bg={"blue.300"}
+    >
+      <NavLink exact to="/">
+        <Flex direction={"row"} align={"center"} gap={2}>
+          <LogoButton />
+          <Text fontFamily={"logo"} fontSize={30} color={"white"}>
+            Study Buddy
+          </Text>
+        </Flex>
+      </NavLink>
+      <ProfileButton user={sessionUser} />
+    </Flex>
+  );
+}
+
+export default Navigation;

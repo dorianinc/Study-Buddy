acda37727c16d6be463173f670a373c61023db67
Dorian Macias <dorianrdba@gmail.com>
2024-08-08 20:02:00 -0700

    delete uneccessary code from annotations controller

--- backend/controllers/annotationsController.js
+++ backend/controllers/annotationsController.js
@@ -81,8 +81,6 @@ const createAnnotation = async (req, res) => {
   }
 };
 
-// add docUrl to annotation
-// create option where you can get annotions through query or direct parameter
 
 // Get all Annotations of specific document
 const getAnnotations = async (req, res, docId = null) => {
@@ -126,55 +124,9 @@ const getAnnotations = async (req, res, docId = null) => {
   }
 };
 
-// Update a single Annotation based off id
-const getSingleAnnotation = async () => {
-  const annotation = await Annotation.findByPk(req.params.docId, { raw: true });
 
-  // check to see if note exists before creating note
-  if (!annotation) res.status(404).json(doesNotExist("Annotation"));
-  else res.status(200).json(annotation);
-};
-
-// Update a single Annotation based off id
-const updateAnnotation = async () => {
-  const { user } = req;
-  const annotation = await Annotation.findByPk(req.params.noteId);
-
-  if (!annotation) res.status(404).json(doesNotExist("Annotation"));
-  else {
-    if (isAuthorized(user.id, annotation.authorId, res)) {
-      for (property in req.body) {
-        let value = req.body[property];
-        annotation[property] = value;
-      }
-      await annotation.save();
-      res.status(200).json(annotation);
-    }
-  }
-};
-
-// Delete a Single Annotation based of id
-const deleteAnnotation = async () => {
-  const { user } = req;
-  const annotation = await Annotation.findByPk(req.params.noteId);
-
-  if (!annotation) res.status(404).json(doesNotExist("Annotation"));
-  else {
-    if (isAuthorized(user.id, annotation.authorId, res)) {
-      await annotation.destroy();
-      res.status(200).json({
-        message: "Successfully deleted",
-        statusCode: 200,
-        noteId: annotation.id,
-      });
-    }
-  }
-};
 
 module.exports = {
   createAnnotation,
   getAnnotations,
-  getSingleAnnotation,
-  updateAnnotation,
-  deleteAnnotation,
 };

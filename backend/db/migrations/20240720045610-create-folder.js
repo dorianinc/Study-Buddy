58c70f8728cabfaff5b6fe14d64b9ec89d32ec36
An Ngo <anng0149@gmail.com>
2024-08-12 00:13:09 -0700

    added options object to migrations

--- backend/db/migrations/20240720045610-create-folder.js
+++ backend/db/migrations/20240720045610-create-folder.js
@@ -35,7 +35,7 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
       },
-    });
+    },options)
   },
   down: async (queryInterface, Sequelize) => {
     options.tableName = 'Folders'

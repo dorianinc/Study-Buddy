4027792fee79219a8f38a8b011ec018b2938182c
Wilmer <wilmer.sampedro@gmail.com>
2024-08-13 18:59:51 -0700

    landing page done

--- frontend/src/components/Folder/MyFolders.jsx
+++ frontend/src/components/Folder/MyFolders.jsx
@@ -4,7 +4,7 @@ import {
   Breadcrumb,
   BreadcrumbItem,
   BreadcrumbLink,
-  Link as ChakraLink, Container, Box, Grid, GridItem, Image, Heading,
+  Link as ChakraLink, Container, Box, Grid, GridItem, Image, Heading, Text
 } from "@chakra-ui/react";
 import Folder from "./Folder";
 import NewFolderButton from "./NewFolderButton";
@@ -12,13 +12,14 @@ import ModalButton from "../Modals/ModalButton";
 import NewFolderModal from "../Modals/NewFolderModal";
 // import { Box, Grid, GridItem } from "@chakra-ui/react";
 import { useGetFoldersQuery } from "../../store/features/api";
+import LandingPage from "../LandingPage/LandingPage";
 
 function MyFolders() {
   const user = useSelector((state) => state.session.user);
   const { data: folders, isLoading, error } = useGetFoldersQuery({ user });
 
   if (isLoading) {
-    return <div>Loading folders...</div>;
+    return <div>Loading...</div>;
   }
 
   return (
@@ -26,18 +27,18 @@ function MyFolders() {
       {user ?
         <Box>
           <Breadcrumb
-          fontWeight={"bold"}
-          fontSize={"xl"}
-          mt={5}
-          mb={5}
-          ml={5}
-          color={"blue.500"}
-        >
-          <BreadcrumbItem isCurrentPage={true}>
-            <BreadcrumbLink>Folders</BreadcrumbLink>
-          </BreadcrumbItem>
-        </Breadcrumb>
-        <Grid templateColumns="repeat(5,1fr)" rowGap={20} p={20}>
+            fontWeight={"bold"}
+            fontSize={"xl"}
+            mt={5}
+            mb={5}
+            ml={5}
+            color={"blue.500"}
+          >
+            <BreadcrumbItem isCurrentPage={true}>
+              <BreadcrumbLink>Folders</BreadcrumbLink>
+            </BreadcrumbItem>
+          </Breadcrumb>
+          <Grid templateColumns="repeat(5,1fr)" rowGap={20} p={20}>
             {error ? (
               <>
                 <div>Error fetching folders: {error.message}</div>
@@ -74,24 +75,7 @@ function MyFolders() {
           </Grid>
         </Box>
         :
-        <>
-          <Box w="100%" display='flex' flexDirection='column' alignItems='center' alignContent='space-between'>
-            <Heading as='h1'>Log in or Sign up to get start with Study Buddy!</Heading>
-            <Grid gridTemplateColumns='1fr 1fr 1fr'>
-
-          <Container boxSize='sm' p={0}>
-            <Image borderRadius='5px' boxSize='500px'  objectFit='contain' src="../../../public/images/logos/highlight-ss.png" alt="highlight example"/>
-          </Container>
-
-          <Container>
-            Click here to log in or sign up
-          </Container>
-          <Container boxSize='lg'>
-            <Image boxSize='500px'  objectFit='contain' src="../../../public/images/logos/highlight-ss-2.png" alt="highlight example"/>
-          </Container>
-            </Grid>
-          </Box>
-        </>
+        <LandingPage />
       }
     </>
   );

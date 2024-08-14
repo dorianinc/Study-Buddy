bf392c55e1eee27b9555464d8bed9503cf633b45
Wilmer <wilmer.sampedro@gmail.com>
2024-08-13 17:44:29 -0700

    in progress

--- frontend/src/components/Folder/MyFolders.jsx
+++ frontend/src/components/Folder/MyFolders.jsx
@@ -1,11 +1,11 @@
 import { useSelector } from "react-redux";
 import { Link as ReactRouterLink } from "react-router-dom";
-import { Link as ChakraLink } from "@chakra-ui/react";
+import { Link as ChakraLink, Container, Box, Grid, GridItem, Image, Heading } from "@chakra-ui/react";
 import Folder from "./Folder";
 import NewFolderButton from "./NewFolderButton";
 import ModalButton from "../Modals/ModalButton";
 import NewFolderModal from "../Modals/NewFolderModal";
-import { Box, Grid, GridItem } from "@chakra-ui/react";
+// import { Box, Grid, GridItem } from "@chakra-ui/react";
 import { useGetFoldersQuery } from "../../store/features/api";
 
 function MyFolders() {
@@ -18,43 +18,64 @@ function MyFolders() {
 
   return (
     <>
-      <Box>
-        <Grid templateColumns="repeat(5,1fr)" rowGap={20} p={20}>
-          {error ? (
-            <>
-              <div>Error fetching folders: {error.message}</div>
-              <GridItem>
-                <ModalButton
-                  className="modal-button auth"
-                  buttonContent={<NewFolderButton />}
-                  modalComponent={<NewFolderModal />}
-                />
-              </GridItem>
-            </>
-          ) : (
-            <>
-              <GridItem>
-                <ModalButton
-                  className="modal-button auth"
-                  buttonContent={<NewFolderButton />}
-                  modalComponent={<NewFolderModal />}
-                />
-              </GridItem>
-              {folders.map((folder) => (
-                <ChakraLink
-                  as={ReactRouterLink}
-                  to={`folders/${folder.id}`}
-                  key={folder.id}
-                >
-                  <GridItem>
-                    <Folder folder={folder} />
-                  </GridItem>
-                </ChakraLink>
-              ))}
-            </>
-          )}
-        </Grid>
-      </Box>
+      {user ?
+        <Box>
+          <Grid templateColumns="repeat(5,1fr)" rowGap={20} p={20}>
+            {error ? (
+              <>
+                <div>Error fetching folders: {error.message}</div>
+                <GridItem>
+                  <ModalButton
+                    className="modal-button auth"
+                    buttonContent={<NewFolderButton />}
+                    modalComponent={<NewFolderModal />}
+                  />
+                </GridItem>
+              </>
+            ) : (
+              <>
+                <GridItem>
+                  <ModalButton
+                    className="modal-button auth"
+                    buttonContent={<NewFolderButton />}
+                    modalComponent={<NewFolderModal />}
+                  />
+                </GridItem>
+                {folders.map((folder) => (
+                  <ChakraLink
+                    as={ReactRouterLink}
+                    to={`folders/${folder.id}`}
+                    key={folder.id}
+                  >
+                    <GridItem>
+                      <Folder folder={folder} />
+                    </GridItem>
+                  </ChakraLink>
+                ))}
+              </>
+            )}
+          </Grid>
+        </Box>
+        :
+        <>
+          <Box w="100%" display='flex' flexDirection='column' alignItems='center' alignContent='space-between'>
+            <Heading as='h1'>Log in or Sign up to get start with Study Buddy!</Heading>
+            <Grid gridTemplateColumns='1fr 1fr 1fr'>
+
+          <Container boxSize='sm' p={0}>
+            <Image borderRadius='5px' boxSize='500px'  objectFit='contain' src="../../../public/images/logos/highlight-ss.png" alt="highlight example"/>
+          </Container>
+
+          <Container>
+            Click here to log in or sign up
+          </Container>
+          <Container boxSize='lg'>
+            <Image boxSize='500px'  objectFit='contain' src="../../../public/images/logos/highlight-ss-2.png" alt="highlight example"/>
+          </Container>
+            </Grid>
+          </Box>
+        </>
+      }
     </>
   );
 }
